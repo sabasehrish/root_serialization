63bcc4bac971647292a4d7e0b3ed0d278110cfa3
Christopher Jones <chrisdjones15@gmail.com>
2021-04-12 12:41:25 -0500

    Added TestProductsOutputer
    Useful for testing in conjunction with TestProductsSource and files
    written from that source.

--- outputerFactoryGenerator.cc
+++ outputerFactoryGenerator.cc
@@ -5,6 +5,7 @@
 #include "DummyOutputer.h"
 #include "TextDumpOutputer.h"
 #include "TBufferMergerRootOutputer.h"
+#include "TestProductsOutputer.h"
 #include "configKeyValuePairs.h"
 
 namespace {
@@ -189,6 +190,8 @@ cce::tf::outputerFactoryGenerator(std::string_view iType, std::string_view iOpti
       return outFactory;
     }
     outFactory = [](unsigned int) { return std::make_unique<TextDumpOutputer>();};
+  } else if(iType == "TestProductsOutputer") {
+    outFactory = [](unsigned int iNLanes) { return std::make_unique<TestProductsOutputer>(iNLanes);};
   }
 
   return outFactory;

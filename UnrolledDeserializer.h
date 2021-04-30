833d6d0899846c1386bace017a6f0536468dedb0
Christopher Jones <chrisdjones15@gmail.com>
2021-04-30 16:50:01 -0500

    Added another deserialize method and made methods const

--- UnrolledDeserializer.h
+++ UnrolledDeserializer.h
@@ -12,10 +12,14 @@ class UnrolledDeserializer {
 public:
   UnrolledDeserializer(TClass*);
 
-  int deserialize(std::vector<char> const& iBuffer, void* iWriteTo) {
+
+  int deserialize(std::vector<char> const& iBuffer, void* iWriteTo) const {
+    return deserialize(&iBuffer.front(), iBuffer.size(), iWriteTo);
+  }
+  int deserialize(char const * iBuffer, size_t iBufferSize, void* iWriteTo) const{
     TBufferFile bufferFile{TBuffer::kRead};
 
-    bufferFile.SetBuffer( const_cast<char*>(&iBuffer.front()), iBuffer.size(), kFALSE);
+    bufferFile.SetBuffer( const_cast<char*>(iBuffer), iBufferSize, kFALSE);
 
     void* address = iWriteTo;
     for(auto& offNSeq: offsetAndSequences_) {

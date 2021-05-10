e1aea11dbdc703c17252966a53d4272d53454368
Kyle Knoepfel <knoepfel@fnal.gov>
2021-05-10 14:24:47 -0500

    Innards for art's EventID.

---
+++ art/ProcessHistoryID.h
@@ -0,0 +1,15 @@
+#ifndef canvas_Persistency_Provenance_ProcessHistoryID_h
+#define canvas_Persistency_Provenance_ProcessHistoryID_h
+
+#include "Hash.h"
+#include "HashedTypes.h"
+
+namespace art {
+  typedef Hash<ProcessHistoryType> ProcessHistoryID;
+}
+
+#endif /* canvas_Persistency_Provenance_ProcessHistoryID_h */
+
+// Local Variables:
+// mode: c++
+// End:
---
+++ art/SortInvalidFirst.h
@@ -0,0 +1,38 @@
+#ifndef canvas_Persistency_Provenance_SortInvalidFirst_h
+#define canvas_Persistency_Provenance_SortInvalidFirst_h
+
+#include <functional>
+
+namespace art {
+  template <typename T>
+  class SortInvalidFirst;
+}
+
+template <typename T>
+class art::SortInvalidFirst : public std::binary_function<T, T, bool> {
+public:
+  SortInvalidFirst() = default;
+  constexpr explicit SortInvalidFirst(T const& invalidValue)
+    : invalidValue_{invalidValue}
+  {}
+  constexpr bool
+  operator()(T const& left, T const& right) const
+  {
+    if (left == invalidValue_ && right != invalidValue_) {
+      return true;
+    } else if (right == invalidValue_ && left != invalidValue_) {
+      return false;
+    } else {
+      return left < right;
+    }
+  }
+
+private:
+  T invalidValue_{};
+};
+
+#endif /* canvas_Persistency_Provenance_SortInvalidFirst_h */
+
+// Local Variables:
+// mode: c++
+// End:

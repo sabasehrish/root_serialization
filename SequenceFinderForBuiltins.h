ad4769b2a3b4bc0f4166ff49c568c1a90bedcf29
Christopher Jones <chrisdjones15@gmail.com>
2021-05-13 13:12:28 -0500

    Added SequenceFinderForBuiltins
    This class is just used to get the TActionSequence needed to store
    a single builtin type.

---
+++ SequenceFinderForBuiltins.h
@@ -0,0 +1,9 @@
+#if !defined(cce_tf_SequenceFinderForBuiltins_h)
+#define cce_tf_SequenceFinderForBuiltins_h
+namespace cce::tf {
+  template<typename T>
+    struct SequenceFinderForBuiltins {
+      T value_;
+    };
+};
+#endif

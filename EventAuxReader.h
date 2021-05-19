52cd4c5a3e89c06a4c0c17690fbb1b0ad1b0432f
Kyle Knoepfel <knoepfel@fnal.gov>
2021-05-19 09:56:33 -0500

    Cleanups for reading art's EventAuxiliary.

--- EventAuxReader.h
+++ EventAuxReader.h
@@ -4,23 +4,22 @@
 #include <cassert>
 
 #include "EventIdentifier.h"
+#include "art/EventAuxiliary.h"
 #include "cms/EventAuxiliary.h"
 
 namespace cce::tf {
 
-inline EventIdentifier cmsEventID(void** address) { 
+inline EventIdentifier cmsEventID(void** address) {
    assert(address);
-   auto aux = *reinterpret_cast<edm::EventAuxiliary**>(address); 
+   auto aux = *reinterpret_cast<edm::EventAuxiliary**>(address);
    return EventIdentifier{aux->run(), aux->luminosityBlock(), aux->event()};
 }
 
-/*
 inline EventIdentifier artEventID(void** address) {
   assert(address);
   auto aux = *reinterpret_cast<art::EventAuxiliary**>(address);
   return EventIdentifier{aux->run(), aux->subRun(), aux->event()};
 }
-*/
 
 class EventAuxReader {
 public:
@@ -29,7 +28,7 @@ public:
   EventIdentifier doWork() {
     if(func_) return func_();
     return EventIdentifier{0,0,0};
-    
+
 
   }
 private:
--- art/EventAuxiliary.h
+++ art/EventAuxiliary.h
@@ -2,17 +2,12 @@
 #define canvas_Persistency_Provenance_EventAuxiliary_h
 // vim: set sw=2 expandtab :
 
-#include "BranchType.h"
 #include "EventID.h"
-#include "ProcessHistoryID.h"
 #include "Timestamp.h"
-
-#include <iosfwd>
 
 namespace art {
   class EventAuxiliary {
   public:
-    static constexpr BranchType branch_type = InEvent;
     enum ExperimentType {
       Any = 0,
       Align = 1,
@@ -24,37 +19,18 @@ namespace art {
       Test = 7
     };
 
-    ~EventAuxiliary();
     EventAuxiliary();
-    EventAuxiliary(EventID const& theId,
-                   Timestamp const& theTime,
-                   bool isReal,
-                   ExperimentType eType = Any);
-    EventAuxiliary(EventAuxiliary const&);
-    EventAuxiliary(EventAuxiliary&&);
-    EventAuxiliary& operator=(EventAuxiliary const&);
-    EventAuxiliary& operator=(EventAuxiliary&&);
 
-    Timestamp const& time() const noexcept;
-    EventID const& id() const noexcept;
-    EventID const& eventID() const noexcept;
-    RunID const& runID() const noexcept;
-    SubRunID const& subRunID() const noexcept;
-    RunNumber_t run() const noexcept;
-    SubRunNumber_t subRun() const noexcept;
-    EventNumber_t event() const noexcept;
-    bool isRealData() const noexcept;
-    EventAuxiliary::ExperimentType experimentType() const noexcept;
-    bool operator==(EventAuxiliary const& other) const noexcept;
-    void write(std::ostream& os) const;
+    RunNumber_t run() const noexcept { return id_.run(); }
+    SubRunNumber_t subRun() const noexcept { return id_.subRun(); }
+    EventNumber_t event() const noexcept { return id_.event(); }
 
   private:
     EventID id_{};
-    Timestamp time_{};
-    bool isRealData_{false};
-    ExperimentType experimentType_{Any};
+    Timestamp time_{}; // Unused
+    bool isRealData_{false}; // Unused
+    ExperimentType experimentType_{Any}; // Unused
   };
-  std::ostream& operator<<(std::ostream&, const EventAuxiliary&);
 } // namespace art
 
 #endif /* canvas_Persistency_Provenance_EventAuxiliary_h */

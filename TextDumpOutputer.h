f78a537fe12acf9e8aa844eda3d1e9558ca09903
Christopher Jones <chrisdjones15@gmail.com>
2021-05-21 07:54:23 -0500

    Made changes from pull request review
    -properly order initialization of member data
    -use class final

--- TextDumpOutputer.h
+++ TextDumpOutputer.h
@@ -5,19 +5,19 @@
 #include "SerialTaskQueue.h"
 
 namespace cce::tf {
-class TextDumpOutputer : public OutputerBase {
+class TextDumpOutputer final : public OutputerBase {
  public:
- TextDumpOutputer(bool perEventDump, bool summaryDump): perEventDump_(perEventDump), summaryDump_(summaryDump), eventCount_{0} {}
+ TextDumpOutputer(bool perEventDump, bool summaryDump): eventCount_{0}, perEventDump_(perEventDump), summaryDump_(summaryDump) {}
 
-  ~TextDumpOutputer() final {}
+  ~TextDumpOutputer() = default;
   
-  void setupForLane(unsigned int iLaneIndex, std::vector<DataProductRetriever> const&) final;
-  void productReadyAsync(unsigned int iLaneIndex, DataProductRetriever const&, TaskHolder iCallback) const final;
+  void setupForLane(unsigned int iLaneIndex, std::vector<DataProductRetriever> const&);
+  void productReadyAsync(unsigned int iLaneIndex, DataProductRetriever const&, TaskHolder iCallback) const;
 
-  void outputAsync(unsigned int iLaneIndex, EventIdentifier const& iEventID, TaskHolder iCallback) const final;
-  bool usesProductReadyAsync() const final {return true;}
+  void outputAsync(unsigned int iLaneIndex, EventIdentifier const& iEventID, TaskHolder iCallback) const;
+  bool usesProductReadyAsync() const {return true;}
 
-  void printSummary() const final;
+  void printSummary() const;
  private:
     mutable SerialTaskQueue queue_;
     std::vector<std::string> productNames_;

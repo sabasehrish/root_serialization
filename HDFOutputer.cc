7d589bc4605d6e79d4083e0790497738b2e130a5
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-09-25 19:41:27 -0700

    Update HDFOutputer.cc

--- HDFOutputer.cc
+++ HDFOutputer.cc
@@ -149,13 +149,13 @@ HDFOutputer::output(EventIdentifier const& iEventID,
       auto [prods, sizes] = get_prods_and_sizes(products_, index, dpi_size);
       register_dataset_timer_start(name.c_str());
       //write_ds<char>(gid, name, prods);
-      write_multidatasets(gid, name.c_str(), &(prods[0]), prods.size(), H5T_NATIVE_CHAR);
+      write_multidatasets(gid, name.c_str(), (char*) &(prods[0]), prods.size(), H5T_NATIVE_CHAR);
       register_dataset_timer_end();
 
       auto s = name+"_sz";
       register_dataset_sz_timer_start(s.c_str());
       //write_ds<size_t>(gid, s, sizes);
-      write_multidatasets(gid, s.c_str(), &(sizes[0]), sizes.size(), H5T_NATIVE_INT);
+      write_multidatasets(gid, s.c_str(), (char*) &(sizes[0]), sizes.size(), H5T_NATIVE_INT);
       register_dataset_sz_timer_end();
     }
     batch_ = 0;

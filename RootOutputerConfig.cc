eab0f4be7ef9ff654cfc98dd16f40ee0a7c759d9
Christopher Jones <chrisdjones15@gmail.com>
2022-01-20 16:33:36 -0600

    Added missing RootOutputerConfig.cc

---
+++ RootOutputerConfig.cc
@@ -0,0 +1,23 @@
+#include "RootOutputerConfig.h"
+#include "ConfigurationParameters.h"
+#include <iostream>
+
+namespace cce::tf {
+  std::optional<std::pair<std::string, RootOutputerConfig>> parseRootConfig(ConfigurationParameters const& params) {
+    auto fileName = params.get<std::string>("fileName");
+    if(not fileName) {
+      std::cout <<"no file name given for Outputer\n";
+      return std::nullopt;
+    }
+
+    RootOutputerConfig config;
+    config.splitLevel_ = params.get<int>("splitLevel",config.splitLevel_);
+
+    config.compressionLevel_ = params.get<int>("compressionLevel", config.compressionLevel_);
+    config.compressionAlgorithm_ = params.get<std::string>("compressionAlgorithm", config.compressionAlgorithm_);
+    config.basketSize_ = params.get<int>("basketSize", config.basketSize_);
+    config.treeMaxVirtualSize_ =  params.get<int>("treeMaxVirtualSize", config.treeMaxVirtualSize_);
+    config.autoFlush_ = params.get<int>("autoFlush", config.autoFlush_);
+    return std::make_pair(*fileName,config);
+  }
+}

e59fa126702347c154fff4a3aa4871539f301511
Christopher Jones <chrisdjones15@gmail.com>
2022-01-28 08:42:52 -0600

    Added new Event based ROOT format handling
    The *RootEvent* classes read/write the format where data products
    are serialized, collected by event and then compressed external to
    the TTree (using the same code as the PDS format).

---
+++ RootEventOutputer.cc
@@ -0,0 +1,287 @@
+#include "RootEventOutputer.h"
+#include "OutputerFactory.h"
+#include "ConfigurationParameters.h"
+#include "UnrolledSerializerWrapper.h"
+#include "SerializerWrapper.h"
+#include "summarize_serializers.h"
+#include "lz4.h"
+#include "zstd.h"
+#include <iostream>
+#include <cstring>
+#include <set>
+
+using namespace cce::tf;
+
+RootEventOutputer::RootEventOutputer(std::string const& iFileName, unsigned int iNLanes, Compression iCompression, int iCompressionLevel, 
+                                     Serialization iSerialization ): 
+  file_(iFileName.c_str(), "recreate", "", 0),
+  serializers_{std::size_t(iNLanes)},
+  compression_{iCompression},
+  compressionLevel_{iCompressionLevel},
+  serialization_{iSerialization},
+  serialTime_{std::chrono::microseconds::zero()},
+  parallelTime_{0}
+  {
+    //gDebug = 3;
+    eventsTree_ = new TTree("Events", "", 0, &file_);
+
+    eventsTree_->Branch("blob", &eventBlob_);
+    eventsTree_->Branch("EventID", &eventID_, "run/i:lumi/i:event/l");
+}
+
+RootEventOutputer::~RootEventOutputer() {
+  file_.Write();
+  file_.Close();
+}
+
+
+void RootEventOutputer::setupForLane(unsigned int iLaneIndex, std::vector<DataProductRetriever> const& iDPs) {
+  auto& s = serializers_[iLaneIndex];
+  switch(serialization_) {
+  case Serialization::kRoot:
+    {   s = SerializeStrategy::make<SerializeProxy<SerializerWrapper>>(); break; }
+  case Serialization::kRootUnrolled:
+    {   s = SerializeStrategy::make<SerializeProxy<UnrolledSerializerWrapper>>(); break; }
+  }
+  s.reserve(iDPs.size());
+  for(auto const& dp: iDPs) {
+    s.emplace_back(dp.name(), dp.classType());
+  }
+}
+
+void RootEventOutputer::productReadyAsync(unsigned int iLaneIndex, DataProductRetriever const& iDataProduct, TaskHolder iCallback) const {
+  auto& laneSerializers = serializers_[iLaneIndex];
+  auto group = iCallback.group();
+  laneSerializers[iDataProduct.index()].doWorkAsync(*group, iDataProduct.address(), std::move(iCallback));
+}
+
+void RootEventOutputer::outputAsync(unsigned int iLaneIndex, EventIdentifier const& iEventID, TaskHolder iCallback) const {
+  auto start = std::chrono::high_resolution_clock::now();
+  auto tempBuffer = std::make_unique<std::vector<uint32_t>>(writeDataProductsToOutputBuffer(serializers_[iLaneIndex]));
+  queue_.push(*iCallback.group(), [this, iEventID, iLaneIndex, callback=std::move(iCallback), buffer=std::move(tempBuffer)]() mutable {
+      auto start = std::chrono::high_resolution_clock::now();
+      const_cast<RootEventOutputer*>(this)->output(iEventID, serializers_[iLaneIndex],*buffer);
+      buffer.reset();
+        serialTime_ += std::chrono::duration_cast<decltype(serialTime_)>(std::chrono::high_resolution_clock::now() - start);
+      callback.doneWaiting();
+    });
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start);
+    parallelTime_ += time.count();
+}
+
+void RootEventOutputer::printSummary() const  {
+  std::cout <<"RootEventOutputer\n  total serial time at end event: "<<serialTime_.count()<<"us\n"
+    "  total parallel time at end event: "<<parallelTime_.load()<<"us\n";
+  summarize_serializers(serializers_);
+}
+
+
+
+void RootEventOutputer::output(EventIdentifier const& iEventID, SerializeStrategy const& iSerializers, std::vector<uint32_t>const& iBuffer) {
+  if(firstTime_) {
+    writeMetaData(iSerializers);
+    firstTime_ = false;
+  }
+  //using namespace std::string_literals;
+  
+  //std::cout <<"   run:"s+std::to_string(iEventID.run)+" lumi:"s+std::to_string(iEventID.lumi)+" event:"s+std::to_string(iEventID.event)+"\n"<<std::flush;
+  
+  //writeEventID(iEventID);
+  eventID_ = iEventID;
+  eventBlob_ = std::move(iBuffer);
+  //std::cout <<"Event "<<eventID_.run<<" "<<eventID_.lumi<<" "<<eventID_.event<<std::endl;
+  //std::cout <<"buffer size "<<eventBlob_.size();
+  //for(auto b: eventBlob_) {
+  //  std::cout <<"   "<<b<<std::endl;
+  //}
+  eventsTree_->Fill();
+  /*
+    for(auto& s: iSerializers) {
+    std::cout<<"   "s+s.name()+" size "+std::to_string(s.blob().size())+"\n" <<std::flush;
+    }
+  */
+}
+
+void RootEventOutputer::writeMetaData(SerializeStrategy const& iSerializers) {
+
+  std::vector<std::pair<std::string, std::string>> typeAndNames;
+  for(auto const& s: iSerializers) {
+    std::string type(s.className());
+    std::string name(s.name());
+    typeAndNames.emplace_back(std::move(type), std::move(name));
+  }
+
+  int objectSerializationUsed = static_cast<std::underlying_type_t<Serialization>>(serialization_);
+
+
+  std::string compression;
+  {
+    //Compression type used
+    // note want exactly 4 bytes so sometimes skip trailing \0
+    switch(compression_) {
+    case Compression::kNone:
+      {
+        compression = "None";
+	break;
+      }
+    case Compression::kLZ4:
+      {
+        compression = "LZ4";
+	break;
+      }
+    case Compression::kZSTD:
+      {
+        compression = "ZSTD";
+	break;
+      }
+    }
+  }
+
+  TTree* meta = new TTree("Meta", "File meta data", 0, &file_);
+  meta->Branch("DataProducts",&typeAndNames, 0, 0);
+  meta->Branch("objectSerializationUsed",&objectSerializationUsed);
+  meta->Branch("compressionAlgorithm",&compression,0,0);
+
+  meta->Fill();
+
+}
+
+std::vector<uint32_t> RootEventOutputer::writeDataProductsToOutputBuffer(SerializeStrategy const& iSerializers) const{
+  //Calculate buffer size needed
+  uint32_t bufferSize = 0;
+  for(auto const& s: iSerializers) {
+    bufferSize +=1+1;
+    auto const blobSize = s.blob().size();
+    bufferSize += bytesToWords(blobSize); //handles padding
+  }
+  //initialize with 0
+  std::vector<uint32_t> buffer(size_t(bufferSize), 0);
+  
+  {
+    uint32_t bufferIndex = 0;
+    uint32_t dataProductIndex = 0;
+    for(auto const& s: iSerializers) {
+      //std::cout <<"  write: "<<s.name()<<std::endl;
+      buffer[bufferIndex++]=dataProductIndex++;
+      auto const blobSize = s.blob().size();
+      uint32_t sizeInWords = bytesToWords(blobSize);
+      buffer[bufferIndex++]=sizeInWords;
+      std::copy(s.blob().begin(), s.blob().end(), reinterpret_cast<char*>( &(*(buffer.begin()+bufferIndex)) ) );
+      bufferIndex += sizeInWords;
+    }
+    assert(buffer.size() == bufferIndex);
+  }
+  int cSize;
+  std::vector<uint32_t> cBuffer = compressBuffer(2, 1, buffer, cSize);
+
+  //std::cout <<"compressed "<<cSize<<" uncompressed "<<buffer.size()*4<<std::endl;
+  //std::cout <<"compressed "<<(buffer.size()*4)/float(cSize)<<std::endl;
+  uint32_t const recordSize = bytesToWords(cSize)+1;
+  cBuffer[0] = recordSize;
+  //Record the actual number of bytes used in the last word of the compression buffer in the lowest
+  // 2 bits of the word
+  cBuffer[1] = buffer.size()*4 + (cSize % 4);
+  if(cBuffer.size() != recordSize+2) {
+    std::cout <<"BAD BUFFER SIZE: want: "<<recordSize+2<<" got "<<cBuffer.size()<<std::endl;
+  }
+  assert(cBuffer.size() == recordSize+2);
+  cBuffer[recordSize+1]=recordSize;
+  return cBuffer;
+}
+
+std::vector<uint32_t> RootEventOutputer::compressBuffer(unsigned int iLeadPadding, unsigned int iTrailingPadding, std::vector<uint32_t> const& iBuffer, int& oCompressedSize) const {
+  switch(compression_) {
+  case Compression::kLZ4 : {
+      return lz4CompressBuffer(iLeadPadding,iTrailingPadding, iBuffer, oCompressedSize);
+    }    
+  case Compression::kNone : {
+      return noCompressBuffer(iLeadPadding, iTrailingPadding, iBuffer, oCompressedSize);
+    } 
+  case Compression::kZSTD : {
+      return zstdCompressBuffer(iLeadPadding, iTrailingPadding, iBuffer, oCompressedSize);
+    }
+  default:
+    return noCompressBuffer(iLeadPadding, iTrailingPadding, iBuffer, oCompressedSize);
+  };
+}
+
+
+std::vector<uint32_t> RootEventOutputer::lz4CompressBuffer(unsigned int iLeadPadding, unsigned int iTrailingPadding, std::vector<uint32_t> const& iBuffer, int& cSize) const {
+  auto const bound = LZ4_compressBound(iBuffer.size()*4);
+  std::vector<uint32_t> cBuffer(bytesToWords(size_t(bound))+iLeadPadding+iTrailingPadding, 0);
+  cSize = LZ4_compress_default(reinterpret_cast<char const*>(&(*iBuffer.begin())), reinterpret_cast<char*>(&(*(cBuffer.begin()+iLeadPadding))), iBuffer.size()*4, bound);
+  cBuffer.resize(bytesToWords(cSize)+iLeadPadding+iTrailingPadding);
+  return cBuffer;
+}
+
+std::vector<uint32_t> RootEventOutputer::noCompressBuffer(unsigned int iLeadPadding, unsigned int iTrailingPadding, std::vector<uint32_t> const& iBuffer, int& cSize) const {
+  auto const bound = iBuffer.size()*4;
+  std::vector<uint32_t> cBuffer(bytesToWords(size_t(bound))+iLeadPadding+iTrailingPadding, 0);
+  cSize = bound;
+  std::copy(iBuffer.begin(), iBuffer.end(), cBuffer.begin()+iLeadPadding);
+  return cBuffer;
+}
+
+std::vector<uint32_t> RootEventOutputer::zstdCompressBuffer(unsigned int iLeadPadding, unsigned int iTrailingPadding, std::vector<uint32_t> const& iBuffer, int& cSize) const {
+  auto const bound = ZSTD_compressBound(iBuffer.size()*4);
+  std::vector<uint32_t> cBuffer(bytesToWords(size_t(bound))+iLeadPadding+iTrailingPadding, 0);
+  cSize = ZSTD_compress(&(*(cBuffer.begin()+iLeadPadding)), bound, &(*iBuffer.begin()),  iBuffer.size()*4, compressionLevel_);
+  if(ZSTD_isError(cSize)) {
+    std::cout <<"ERROR in comparession "<<ZSTD_getErrorName(cSize)<<std::endl;
+  }
+  cBuffer.resize(bytesToWords(cSize)+iLeadPadding+iTrailingPadding);
+  return cBuffer;
+}
+
+namespace {
+
+  class PDSMaker : public OutputerMakerBase {
+  public:
+    PDSMaker(): OutputerMakerBase("RootEventOutputer") {}
+
+    std::unique_ptr<OutputerBase> create(unsigned int iNLanes, ConfigurationParameters const& params) const final {
+
+      auto fileName = params.get<std::string>("fileName");
+      if(not fileName) {
+        std::cout <<"no file name given for RootEventOutputer\n";
+        return {};
+      }
+
+      int compressionLevel = params.get<int>("compressionLevel", 18);
+
+      auto compressionName = params.get<std::string>("compressionAlgorithm", "ZSTD");
+      auto serializationName = params.get<std::string>("serializationAlgorithm", "ROOT");
+
+      RootEventOutputer::Compression compression = RootEventOutputer::Compression::kZSTD;
+
+      if(compressionName == "" or compressionName =="None") {
+        compression = RootEventOutputer::Compression::kNone;
+      } else if (compressionName == "LZ4") {
+        compression = RootEventOutputer::Compression::kLZ4;
+      } else if (compressionName == "ZSTD") {
+      compression = RootEventOutputer::Compression::kZSTD;
+      } else {
+        std::cout <<"unknown compression "<<compressionName<<std::endl;
+        return {};
+      }
+
+      RootEventOutputer::Serialization serialization = RootEventOutputer::Serialization::kRoot;
+
+      if(serializationName == "" or serializationName=="ROOT") {
+        serialization = RootEventOutputer::Serialization::kRoot;
+      } else if(serializationName == "ROOTUnrolled" or serializationName=="Unrolled") {
+        serialization = RootEventOutputer::Serialization::kRootUnrolled;
+      } else {
+        std::cout <<"unknown serialization "<<serializationName<<std::endl;
+        return {};
+      }
+      
+      return std::make_unique<RootEventOutputer>(*fileName,iNLanes, compression, compressionLevel, serialization);
+    }
+    
+  };
+
+  PDSMaker s_maker;
+}
+
+

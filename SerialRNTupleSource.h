f4319f6d9df24103c5d37ec67cafab4642549e56
Christopher Jones <chrisdjones15@gmail.com>
2024-02-23 09:39:38 -0600

    Added delayed reading ability to SerialRNTupleSource
    Also updated to newest RNTuple API

--- SerialRNTupleSource.h
+++ SerialRNTupleSource.h
@@ -14,9 +14,9 @@
 #include "ROOT/RNTuple.hxx"
 
 namespace cce::tf {
-  class SerialRNTupleDelayedRetriever : public DelayedProductRetriever {
+  class SerialRNTuplePromptRetriever : public DelayedProductRetriever {
   public:
-    SerialRNTupleDelayedRetriever(SerialTaskQueue* iQueue,
+    SerialRNTuplePromptRetriever(SerialTaskQueue* iQueue,
                                   ROOT::Experimental::REntry* iEntry,
                                   std::vector<std::string> const* iFieldIDs):
       queue_(iQueue), entry_(iEntry), fieldIDs_(iFieldIDs),
@@ -31,9 +31,32 @@ namespace cce::tf {
     std::chrono::microseconds accumulatedTime_;
   };
 
+  class SerialRNTupleDelayedRetriever : public DelayedProductRetriever {
+  public:
+    SerialRNTupleDelayedRetriever(SerialTaskQueue* iQueue,
+                                  ROOT::Experimental::RNTupleReader& iReader,
+                                  std::vector<std::string> const& iFieldIDs,
+                                  std::vector<void*>* iProductPtrs):
+      queue_(iQueue), addresses_(iProductPtrs), accumulatedTime_{std::chrono::microseconds::zero()}{
+      fillViews(iReader, iFieldIDs);
+    }
+
+    void setEventIndex(std::uint64_t iIndex) { eventIndex_ = iIndex;}
+    void getAsync(DataProductRetriever&, int index, TaskHolder) final;
+    std::chrono::microseconds accumulatedTime() const { return accumulatedTime_;}
+
+  private:
+    void fillViews(ROOT::Experimental::RNTupleReader& iReader, std::vector<std::string> const& iFieldIDs);
+    SerialTaskQueue* queue_;
+    std::vector<ROOT::Experimental::RNTupleView<void>> views_;
+    std::vector<void*>* addresses_;
+    std::chrono::microseconds accumulatedTime_;
+    std::uint64_t eventIndex_ = 0;
+  };
+
   class SerialRNTupleSource : public SharedSourceBase {
   public:
-    SerialRNTupleSource(unsigned iNLanes, unsigned long long iNEvents, std::string const& iName);
+    SerialRNTupleSource(unsigned iNLanes, unsigned long long iNEvents, std::string const& iName, bool iDelayReading);
     size_t numberOfDataProducts() const final {return dataProductsPerLane_[0].size();}
 
     std::vector<DataProductRetriever>& dataProducts(unsigned int iLane, long iEventIndex) final {
@@ -57,10 +80,13 @@ namespace cce::tf {
 
     //per lane items
     std::vector<std::unique_ptr<ROOT::Experimental::REntry>> entries_;
+    std::vector<SerialRNTuplePromptRetriever> promptReaders_;
     std::vector<SerialRNTupleDelayedRetriever> delayedReaders_;
     std::vector<EventIdentifier> identifiers_;
     std::vector<std::vector<void*>> ptrToDataProducts_;
     std::vector<std::vector<DataProductRetriever>> dataProductsPerLane_;
+
+    bool delayReading_;
   };
 }
 
